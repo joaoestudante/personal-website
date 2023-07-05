507a03eeca98a0d6ecf4f38288a0f90796d219a4
João Lourenço <estudante.lourenco@gmail.com>
2023-07-05 22:42:55 +0000

    Add splatoon first post - wip

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,22 @@ import tailwind from "@astrojs/tailwind";
 import prefetch from "@astrojs/prefetch";
 import image from "@astrojs/image";
 
-import mdx from "@astrojs/mdx";
+import mdx from '@astrojs/mdx';
+import m2dx from 'astro-m2dx';
+
+/** @type {import('astro-m2dx').Options} */
+const m2dxOptions = {
+  normalizePaths: true,
+  exportComponents: true
+};
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [svelte(), tailwind(), prefetch(), image(), mdx()],
+  markdown: {
+    remarkPlugins: [[m2dx, m2dxOptions]],
+    //               ^^^^
+    extendDefaultPlugins: true,
+  },
   site: 'https://joaoestudante.com'
 });
