312ebc9ee9d74417cf911c698d1b32a8e7266b94
João Lourenço <estudante.lourenco@gmail.com>
2023-09-24 17:38:41 +0000

    Display photos correctly

--- src/components/PhotoGrid.svelte
+++ src/components/PhotoGrid.svelte
@@ -3,9 +3,16 @@
   export let photosUrls = [];
   console.log(photosUrls);
   const photosForGallery = photosUrls.map((photoUrl) => {
+    const splitUrl = photoUrl.default.src.split("/");
     return {
-      src: photoUrl.default.src,
+      src:
+        "/" +
+        splitUrl
+          .splice(splitUrl.indexOf("public") + 1, splitUrl.length)
+          .join("/"),
       alt: "Photo",
+      width: photoUrl.default.width / 5,
+      height: photoUrl.default.height / 5,
     };
   });
   // create as many arrays as needed to fit 3 items per row
@@ -19,21 +26,26 @@
   function makeImageFullScreen(el) {
     document.getElementById("fullpage").classList.toggle("hidden");
     document.getElementById(
-      "fullpage"
-    ).innerHTML = `<img id="fullpage_img" src="${el.target.src}" class="max-h-full max-w-full object-contain">`;
+      "fullpage_content"
+    ).innerHTML = `<img id="fullpage_img" src="${el.target.src}" class="max-h-full max-w-full object-contain rounded-md">`;
     document.body.style.overflow = "hidden";
+
+    const splitUrl = el.target.src.split("/");
+    currentSelectedSrc =
+      "/" +
+      splitUrl.splice(splitUrl.indexOf("images"), splitUrl.length).join("/");
   }
 
   function closeFullscreenImage(el) {
     document.getElementById("fullpage").classList.toggle("hidden");
-    document.getElementById("fullpage").innerHTML = "";
+    document.getElementById("fullpage_content").innerHTML = "";
     document.body.style.overflow = "auto";
+    currentSelectedSrc = "";
   }
 
   function showNextImage() {
-    let currentImage = document.getElementById("fullpage_img").src;
     let currentImageIndex = photosForGallery.findIndex(
-      (photo) => photo.src === currentImage
+      (photo) => photo.src === currentSelectedSrc
     );
     let nextImageIndex = currentImageIndex + 1;
     if (nextImageIndex === photosForGallery.length) {
@@ -41,13 +53,12 @@
     }
     document.getElementById("fullpage_img").src =
       photosForGallery[nextImageIndex].src;
+    currentSelectedSrc = photosForGallery[nextImageIndex].src;
   }
 
   function showPreviousImage() {
-    // Consider only the src starting from the @ symbol
-    let currentImage = document.getElementById("fullpage_img").src;
     let currentImageIndex = photosForGallery.findIndex(
-      (photo) => photo.src === currentImage
+      (photo) => photo.src === currentSelectedSrc
     );
     let previousImageIndex = currentImageIndex - 1;
     if (previousImageIndex === -1) {
@@ -55,14 +66,7 @@
     }
     document.getElementById("fullpage_img").src =
       photosForGallery[previousImageIndex].src;
-  }
-
-  let touchstartX = 0;
-  let touchendX = 0;
-
-  function checkDirection() {
-    if (touchendX < touchstartX) alert("swiped left!");
-    if (touchendX > touchstartX) alert("swiped right!");
+    currentSelectedSrc = photosForGallery[previousImageIndex].src;
   }
 
   onMount(() => {
@@ -74,33 +78,65 @@
         case 39:
           showNextImage();
           break;
+        // on esc, close
+        case 27:
+          closeFullscreenImage();
       }
     };
-    document.addEventListener("touchstart", (e) => {
-      touchstartX = e.changedTouches[0].screenX;
-    });
-
-    document.addEventListener("touchend", (e) => {
-      touchendX = e.changedTouches[0].screenX;
-      checkDirection();
-    });
   });
 </script>
 
 <div class="columns-1 sm:columns-2 md:columns-3 lg:columns-4 gap-4">
-  {#each photosForGallery as photo}
-    <!-- svelte-ignore a11y-click-events-have-key-events -->
-    <img
-      src={photo.src}
-      alt={photo.alt}
-      on:click={makeImageFullScreen}
-      class="w-full h-auto rounded-lg mb-4 hover:scale-105 cursor-zoom-in"
-      loading="lazy"
-    />
-  {/each}
+  <div on:click={makeImageFullScreen}>
+    {#each photosForGallery as photo}
+      <img
+        src={photo.src}
+        alt={photo.alt}
+        class="w-full h-auto rounded-lg mb-4 hover:scale-105 cursor-zoom-in"
+        loading="lazy"
+        width={photo.width}
+        height={photo.height}
+      />
+    {/each}
+  </div>
   <div
     id="fullpage"
-    on:click={closeFullscreenImage}
-    class="after:content-['Click_anywhere_to_close'] after:bottom-10 after:text-center after:text-sm after:absolute after:text-white flex justify-center md:p-8 p-2 hidden fixed z-50 top-0 cursor-zoom-out left-0 w-screen h-screen bg-opacity-95 bg-black"
-  />
+    class="flex flex-wrap justify-between pt-2 pb-2 hidden fixed z-50 top-0 left-0 w-screen h-screen bg-opacity-95 bg-black"
+  >
+    <div
+      id="fullpage_content"
+      class="flex justify-center w-full h-full items-center cursor-zoom-out py-8 px-8"
+      on:click={closeFullscreenImage}
+    />
+    <!-- <div class="flex items-center cursor-pointer w-25" on:click={showPreviousImage}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="32"
+        height="32"
+        fill="currentColor"
+        class="text-white text-3xl"
+        viewBox="0 0 16 16"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"
+        />
+      </svg>
+    </div>
+    <div class="flex items-center cursor-pointer w-25" on:click={showNextImage}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="32"
+        height="32"
+        fill="currentColor"
+        class="text-white text-3xl"
+        viewBox="0 0 16 16"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
+        />
+      </svg>
+    </div> -->
+  </div>
 </div>
