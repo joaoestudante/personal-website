d7beb1b35ec3acfc71928cd9003e90416457e71b
João Lourenço <estudante.lourenco@gmail.com>
2025-04-06 15:07:32 +0100

    Projects list page and navbar update

--- src/components/NavBar.svelte
+++ src/components/NavBar.svelte
@@ -2,21 +2,51 @@
   import DarkmodeSwitcher from "../components/DarkmodeSwitcher.svelte";
   export let page = "/";
   const navLinks = [
-    { title: "Home", link: "/", active: page == "/" },
+    {
+      title: "Projects",
+      link: "/projects",
+      active: page.includes("/projects"),
+    },
     { title: "Blog", link: "/blog", active: page.includes("/blog") },
     { title: "Photos", link: "/photos", active: page.includes("/photos") },
   ];
+  import profilePic from "./profile-pic.png";
 </script>
 
-<nav class="flex flex-row flex-wrap py-4 justify-end items-center gap-3 mb-12">
-  {#each navLinks as navLink}
+<nav class="flex flex-row flex-wrap justify-between mb-12 mt-4">
+  {#if page != "/"}
     <a
-      class="hover:bg-gray-100 dark:hover:bg-gray-700 rounded-lg p-2 dark:text-slate-200 md:text-xl last:mr-0sm:mx-4"
-      class:text-primary-700={navLink.active}
-      class:dark:text-primary-600={navLink.active}
-      class:dark:text-slate-200={!navLink.active}
-      href={navLink.link}>{navLink.title}</a
+      href="/"
+      class="flex gap-6 items-center hover:bg-gray-200 rounded-lg px-4 cursor-pointer"
     >
-  {/each}
-  <DarkmodeSwitcher></DarkmodeSwitcher>
+      <img
+        src={profilePic.src}
+        alt="Profile"
+        class="rounded-full"
+        height="50px"
+        width="50px"
+      />
+      <h3
+        class="font-display text-lg font-extrabold text-gray-900 dark:text-white"
+      >
+        João Estudante
+      </h3>
+    </a>
+  {/if}
+  {#if page == "/"}
+    <div></div>
+  {/if}
+
+  <div class="flex flex-row flex-wrap py-4 justify-end items-center gap-3">
+    {#each navLinks as navLink}
+      <a
+        class="hover:bg-gray-100 dark:hover:bg-gray-700 rounded-lg p-2 dark:text-slate-200 md:text-xl last:mr-0sm:mx-4"
+        class:text-primary-700={navLink.active}
+        class:dark:text-primary-600={navLink.active}
+        class:dark:text-slate-200={!navLink.active}
+        href={navLink.link}>{navLink.title}</a
+      >
+    {/each}
+    <DarkmodeSwitcher></DarkmodeSwitcher>
+  </div>
 </nav>
---
+++ src/components/Projects.svelte
@@ -0,0 +1,31 @@
+<script>
+  export let projects = [];
+</script>
+
+<div>
+  <h3 class="text-3xl font-bold tracking-tight text-gray-900 dark:text-white">
+    Projects
+  </h3>
+  <div class="flex flex-col gap-4 mt-8 mb-24">
+    <div class="flex flex-row flex-wrap gap-4 justify-between">
+      {#each projects as project}
+        <a
+          href={project.url}
+          class="w-full h-48 flex gap-4 bg-white dark:bg-gray-700 shadow-sm border hover:scale-105 transition-all cursor-pointer dark:border-0 rounded-[2em]"
+        >
+          <img
+            src={project.frontmatter.image}
+            class="w-48"
+            alt="project cover"
+          />
+          <div class="flex flex-col gap-4 mt-4">
+            <p class="font-bold text-lg">{project.frontmatter.title}</p>
+            <p class="font-light">
+              {project.frontmatter.description}
+            </p>
+          </div>
+        </a>
+      {/each}
+    </div>
+  </div>
+</div>
