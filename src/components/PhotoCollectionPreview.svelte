28e8ce7f601b4a5596aa8f6662100e4de35c4426
João Lourenço <estudante.lourenco@gmail.com>
2025-04-25 10:37:48 +0100

    Add cat photos and more favourites

--- src/components/PhotoCollectionPreview.svelte
+++ src/components/PhotoCollectionPreview.svelte
@@ -1,20 +1,34 @@
 <script>
-    import { Card, Badge } from "flowbite-svelte";
-    export let collectionTitle='';
-    export let collectionDate = '';
-    export let collectionDescription = '';
-    export let collectionUrl = '';
-    export let collectionPreviewImgUrl = '';
+  import { Card, Badge } from "flowbite-svelte";
+  export let collectionTitle = "";
+  export let collectionDate = "";
+  export let collectionDescription = "";
+  export let collectionUrl = "";
+  export let collectionPreviewImgUrl = "";
 </script>
 
-
-<a href="{collectionUrl}" class="block w-96 h-96 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-    <img src="{collectionPreviewImgUrl}" alt="{collectionTitle}" class="w-full h-48 object-cover rounded-t-lg shadow" />
-    <div class="p-6">
-        <h4 class="font-display text-xl font-semibold lg:text-2xl text-gray-900 dark:text-white">{collectionTitle}</h4>
-        <p class="text-gray-500 dark:text-gray-300 italic">{collectionDate.toISOString().split('T')[0]}</p>
-        <p class="mt-4 font-normal text-gray-500 dark:text-gray-300 text-lg lg:text-xl ">
-        {collectionDescription}
-        </p>
-    </div>
+<a
+  href={collectionUrl}
+  class="block grow h-96 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+>
+  <img
+    src={collectionPreviewImgUrl}
+    alt={collectionTitle}
+    class="w-full h-48 object-cover rounded-t-lg shadow"
+  />
+  <div class="p-6">
+    <h4
+      class="font-display text-xl font-semibold lg:text-2xl text-gray-900 dark:text-white"
+    >
+      {collectionTitle}
+    </h4>
+    <p class="text-gray-500 dark:text-gray-300 italic">
+      {collectionDate.toISOString().split("T")[0]}
+    </p>
+    <p
+      class="mt-4 font-normal text-gray-500 dark:text-gray-300 text-lg lg:text-xl"
+    >
+      {collectionDescription}
+    </p>
+  </div>
 </a>
--- src/components/Photos.svelte
+++ src/components/Photos.svelte
@@ -1,25 +1,28 @@
 <script>
-    import PhotoCollectionPreview from "./PhotoCollectionPreview.svelte";
-    export let collections = [];
+  import PhotoCollectionPreview from "./PhotoCollectionPreview.svelte";
+  export let collections = [];
 </script>
 
 <div>
-    <h3 class="text-3xl font-bold tracking-tight text-gray-900 dark:text-white">Photos</h3>
-    {#if collections.length == 0}
-    <p class="mt-4 text-gray-900 dark:text-white">There's nothing here yet :)</p>
-    {/if}
-    <div class="flex flex-col gap-4 mt-8 mb-24">
-        <div class="flex flex-row flex-wrap gap-4 justify-between">
-          {#each collections as collection}
-          <PhotoCollectionPreview
-              collectionTitle={collection.frontmatter.title}
-              collectionUrl={collection.url}
-              collectionDescription={collection.frontmatter.description}
-              collectionDate={new Date(collection.frontmatter.pubDate)}
-              collectionPreviewImgUrl={collection.frontmatter.previewImgUrl}
-              >
-            </PhotoCollectionPreview>
-          {/each}
-        </div>
+  <h3 class="text-3xl font-bold tracking-tight text-gray-900 dark:text-white">
+    Photos
+  </h3>
+  {#if collections.length == 0}
+    <p class="mt-4 text-gray-900 dark:text-white">
+      There's nothing here yet :)
+    </p>
+  {/if}
+  <div class="flex flex-col gap-4 mt-8 mb-24">
+    <div class="flex flex-row flex-wrap gap-4">
+      {#each collections as collection}
+        <PhotoCollectionPreview
+          collectionTitle={collection.frontmatter.title}
+          collectionUrl={collection.url}
+          collectionDescription={collection.frontmatter.description}
+          collectionDate={new Date(collection.frontmatter.pubDate)}
+          collectionPreviewImgUrl={collection.frontmatter.previewImgUrl}
+        ></PhotoCollectionPreview>
+      {/each}
     </div>
+  </div>
 </div>
